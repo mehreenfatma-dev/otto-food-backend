19bcde43bae82093419b19564c1bbdcacb11056a
Mehreen Fatima <mehreenf202@gmail.com>
2026-06-24 12:19:48 -0800

    Debug Railway env

--- config/database.js
+++ config/database.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
     await mongoose.connect(process.env.MONGODB_URI);
+
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
